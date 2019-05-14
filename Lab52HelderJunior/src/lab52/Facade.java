c1e767e497afe69b2d7b8748899236f1c64eb8c5
Helder Chaves Leite Junior <helder.junior@ccc.ufcg.edu.br>
2019-05-14 09:56:14 -0300

    Adição das classes de teste

---
+++ Lab52HelderJunior/src/lab52/Facade.java
@@ -0,0 +1,11 @@
+package lab52;
+
+import easyaccept.EasyAccept;
+
+public class Facade
+{
+    public static void main(String[] args)
+    {
+        args = new String[] {"lab52.Facade", "acceptance_test/use_case_1.txt", "use_case_2.txt", "use_case_1.txt"}
+    }
+}
